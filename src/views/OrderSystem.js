c97b0bb735bc9b2d3f742a05b46aa6995d2a62da
qiaozp <chivalry.pp@gmail.com>
2021-06-15 00:41:38 +0800

    fix typo & file name

--- src/views/OrderSystem.js
+++ src/views/OrderSystem.js
@@ -1,9 +1,9 @@
 import {List, Button, Space} from 'antd';
 import Layout, {Content} from "antd/es/layout/layout";
 import {DeleteOutlined, FileSearchOutlined} from '@ant-design/icons';
-import $http from "../Utils";
+import $http from "../Utils/httpUtil";
 import {useEffect, useState} from "react";
-import {imgScrPrefix} from "../Utils/GlobalVariableConfig";
+import {imgSrcPrefix} from "../Utils/GlobalVariableConfig";
 import storage from "sweet-storage";
 import {useHistory} from "react-router-dom";
 
@@ -23,7 +23,7 @@ const OrderList = () => {
                         test.push({
                             key: item['car']['name'],
                             carId: item['carId'],
-                            imgSrc: imgScrPrefix + item['car']['images'][0]
+                            imgSrc: imgSrcPrefix + item['car']['images'][0]
                         })
                     })
                     setData(test)
